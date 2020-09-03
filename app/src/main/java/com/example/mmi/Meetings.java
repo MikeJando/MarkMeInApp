64c1944bc315970a1988cd2f7b1e8a281fbbed9a
msjando <msjando@neiu.edu>
2020-09-02 23:26:17 -0500

    Updated AttendanceListActivity and created the Meetings Object

---
+++ app/src/main/java/com/example/mmi/Meetings.java
@@ -0,0 +1,15 @@
+package com.example.mmi;
+
+public class Meetings
+{
+        public String classDate;
+
+        public Meetings(String classDate) {
+            this.classDate = classDate;
+        }
+
+        public String getClassDate() {
+            return classDate;
+        }
+}
+
