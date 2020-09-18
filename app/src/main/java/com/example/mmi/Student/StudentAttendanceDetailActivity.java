031355ff6e7619f51ff280ed805391eb4138181a
msjando <msjando@neiu.edu>
2020-09-17 21:21:55 -0500

    Added StudentMeetingLIstActivity

--- app/src/main/java/com/example/mmi/Student/StudentAttendanceDetailActivity.java
+++ app/src/main/java/com/example/mmi/Student/StudentAttendanceDetailActivity.java
@@ -5,10 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.mmi.DBUtility;
 import com.example.mmi.R;
 import org.json.JSONObject;
